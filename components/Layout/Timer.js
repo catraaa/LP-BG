0b2487b5a8c29fed9684a002312ef0e54c6f129a
Beni Tiyas Kristanti <tiyaskristanti12@gmai.com>
2025-07-04 06:30:51 +0700

    mobile

--- components/Layout/Timer.js
+++ components/Layout/Timer.js
@@ -22,28 +22,34 @@ const FloatingMarketing = () => {
   return (
     <div className="fixed bottom-24 right-4 md:bottom-14 md:right-10 z-50">
       {open && (
-        <div className="bg-white-500 shadow-xl rounded-xl p-4 mb-3 border border-blue-500 w-[52] md:w-64">
-          <h4 className="font-bold text-center text-black-500 mb-3">Marketing</h4>
+        <div className="bg-white shadow-xl rounded-xl p-4 mb-3 border border-blue-500 w-64 max-w-[90vw]">
+          <h4 className="font-bold text-center text-black mb-3 text-sm md:text-base">Marketing</h4>
           {marketings.map((item) => (
             <a
               key={item.id}
               href={`https://wa.me/${item.number}`}
               target="_blank"
               rel="noopener noreferrer"
-              className="flex items-center gap-2 bg-blue-500 hover:bg-green-700 text-white-500 text-sm font-medium px-4 py-2 rounded-md mb-2 transition-all w-52"
+              className="flex items-center gap-2 bg-blue-500 hover:bg-green-700 text-white text-sm font-medium px-4 py-2 rounded-md mb-2 transition-all w-full"
             >
-              <Image src="/assets/Icon/wa.png" alt="WA" width={20} height={20} />
-              {item.label}
+              <Image
+                src="/assets/Icon/wa.png"
+                alt="WA"
+                width={20}
+                height={20}
+                className="w-5 h-5"
+              />
+              <span className="truncate">{item.label}</span>
             </a>
           ))}
         </div>
       )}
       <button
         onClick={() => setOpen(!open)}
-        className="bg-hijau-500 hover:bg-hijau-500 text-white-500 font-semibold px-5 py-3 rounded-full shadow-lg transition-all flex items-center gap-2 text-sm md:text-base"
+        className="bg-hijau-500 hover:bg-hijau-500 text-white font-semibold px-5 py-3 rounded-full shadow-lg transition-all flex items-center gap-2 text-sm md:text-base w-full max-w-[12rem]"
       >
         <Image src="/assets/Icon/wa.png" alt="Chat Icon" width={24} height={24} />
-        <span>Contact Us</span>
+        <span className="whitespace-nowrap">Contact Us</span>
       </button>
     </div>
   );
--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://www.saranateknikbali.com', // Ganti dengan domain Anda
+    siteUrl: 'https://www.bagongbirupariwisata.com', // Ganti dengan domain Anda
     generateRobotsTxt: true, // Menghasilkan robots.txt secara otomatis
     outDir: './public', // Lokasi output file sitemap dan robots.txt
     exclude: ['/404'], // Mengecualikan halaman 404
