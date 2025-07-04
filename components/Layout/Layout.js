e2cac8ae44796f7db62508bb82929b74a2d21661
Beni Tiyas Kristanti <tiyaskristanti12@gmai.com>
2025-07-04 07:42:26 +0700

    bismillah

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,27 +1,25 @@
 import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
-import Timer from "./Timer"; // isinya FloatingMarketing
+import Timer from "./Timer";
 import { useRouter } from "next/router";
 
+// Layout.js
 const Layout = ({ children }) => {
   const router = useRouter();
   const isAdminRoute = router.pathname.startsWith("/admin");
 
   return (
     <>
-      {/* Header hanya tampil kalau bukan route admin */}
       {!isAdminRoute && <Header />}
-
-      {/* Timer (FloatingMarketing) dipindah ke sini */}
-      {!isAdminRoute && <Timer />}
-
-      {/* Konten utama */}
-      <div className={isAdminRoute ? "" : "relative z-0"}>
+      {!isAdminRoute && (
+        <div className="fixed top-0 w-full z-50">
+          <Timer />
+        </div>
+      )}
+      <div className={isAdminRoute ? "" : "relative z-10"}>
         {children}
       </div>
-
-      {/* Footer hanya tampil kalau bukan route admin */}
       {!isAdminRoute && <Footer />}
     </>
   );
